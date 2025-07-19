02c47b64aa5c963e423192715834de32eafa86b9
dhanudevopsworld <dhanudevopsworld@gmail.com>
2025-07-19 12:41:42 -0700

    Add files via upload

---
+++ EC2-INSTANCE/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 6.4"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+  profile = "terraformprofile"
+}
+
+resource "aws_instance" "tomcat" {
+  ami = "ami-0a1235697f4afa8a4"
+  instance_type = "t2.micro"
+  tags = {
+    "Name" = "Tomcat_server"
+  }
+}
